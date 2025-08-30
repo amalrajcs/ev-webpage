5be2958e881f13c5e5aeb2f0bdf15cfffa36d277
amalrajcs <amalrajcs712@gmail.com>
2025-08-30 13:47:24 +0530

    Responsive Chnaged

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,5 +1,5 @@
 import './Hero.css'
-import  arrow_btn from '../../assets/arrow_btn.png'
+import arrow_btn from '../../assets/arrow_btn.png'
 import pause_icon from '../../assets/pause_icon.png'
 import play_icon from '../../assets/play_icon.png'
 
